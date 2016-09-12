8c2ba05a3fdc06057727f9ae26bf60cc7d282daa
Caio <caiocanic@outlook.com>
2016-09-12 23:33:38 +0000

    Now call the other scripts correctly.

--- doAll.sh
+++ doAll.sh
@@ -3,7 +3,9 @@
 read -p "Where do you want to start the build: " -e dir
 export cmHome=$dir/cm13.0
 
+securegen="$(dirname "$(pwd)")"
+
 sh newBuild.sh
-sh visualId/replaceId.sh
-sh blobs/addBlobs.sh
-sh apps/addPacks.sh
+(cd $securegen/visualId; bash replaceId.sh)
+(cd $securegen/blobs/; bash addBlobs.sh)
+(cd $securegen/apps/; bash addPacks.sh)
