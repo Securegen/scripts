1f6466034d1b6c6e13e70d796ef706482dc095b5
Caio <caiocanic@outlook.com>
2016-10-19 19:57:39 +0000

    doAll.sh now can receive a path as parameter.
    doAll.sh has been updated so it can receive the path to where to start the new build as parameter. This will be used to automate the build process in the future.

--- doAll.sh
+++ doAll.sh
@@ -1,11 +1,16 @@
 #!/bin/bash -e
 
-read -p "Where do you want to start the build: " -e dir
-export cmHome=$dir/cm13.0
+if [ ! -z "$1" ]; then
+	export cmHome=$1/cm13.0
+	echo "A new build will be started on $cmHome"
+else
+	read -p "Where do you want to start the build: " -e dir
+	export cmHome=$dir/cm13.0
+fi
 
 securegen="$(dirname "$(pwd)")"
 
-sh newBuild.sh
+bash newBuild.sh
 (cd $securegen/visualId; bash replaceId.sh)
-(cd $securegen/blobs/; bash addBlobs.sh)
 (cd $securegen/apps/; bash addPacks.sh)
+(cd $securegen/blobs/; bash addBlobs.sh)
