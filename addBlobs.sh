032a7ce895e28a1ec471f23c944eda986655c81f
Caio <caiocanic@outlook.com>
2016-11-11 18:45:48 -0200

    Fixed some var and path names

--- addBlobs.sh
+++ addBlobs.sh
@@ -10,28 +10,28 @@ if [ ! -d "$securegenBlobs" ]; then
 	mkdir $securegenBlobs
 fi
 
-if [ ! -d "$securenBlobs/proprietary_vendor_lge" ]; then
+if [ ! -d "$securegenBlobs/proprietary_vendor_lge" ]; then
 	echo "Missing LG blobs."
 	echo "Downloading..."
 	git clone https://github.com/TheMuppets/proprietary_vendor_lge.git $securegenBlobs/proprietary_vendor_lge
 	echo "Done."
 fi
 
-if [ ! -d "$securenBlobs/proprietary_vendor_motorola" ]; then
+if [ ! -d "$securegenBlobs/proprietary_vendor_motorola" ]; then
 	echo "Missing Motorola blobs."
 	echo "Downloading..."
 	git clone https://github.com/TheMuppets/proprietary_vendor_motorola.git $securegenBlobs/proprietary_vendor_motorola
 	echo "Done."
 fi
 
-if [ ! -d "$securenBlobs/proprietary_vendor_samsung" ]; then
+if [ ! -d "$securegenBlobs/proprietary_vendor_samsung" ]; then
 	echo "Missing Samsung blobs."
 	echo "Downloading..."
 	git clone https://github.com/TheMuppets/proprietary_vendor_samsung.git $securegenBlobs/proprietary_vendor_samsung
 	echo "Done."
 fi
 
-if [ ! -d "$securenBlobs/proprietary_vendor_asus" ]; then
+if [ ! -d "$securegenBlobs/proprietary_vendor_asus" ]; then
 	echo "Missing Asus blobs."
 	echo "Downloading..."
 	git clone https://github.com/TheMuppets/proprietary_vendor_asus.git $securegenBlobs/proprietary_vendor_asus
@@ -61,30 +61,30 @@ mkdir $cmVendor/motorola
 
 echo "Adding peregrine..."
 breakfast peregrine
-cp -R $securegenBlobs/motorola/msm8226-common $cmVendor/motorola
-cp -R $securegenBlobs/motorola/peregrine $cmVendor/motorola
+cp -R $securegenBlobs/proprietary_vendor_motorola/msm8226-common $cmVendor/motorola
+cp -R $securegenBlobs/proprietary_vendor_motorola/peregrine $cmVendor/motorola
 breakfast peregrine
 echo "Done."
 echo "peregrine has been added."
 
 echo "Adding osprey..."
 breakfast osprey
-cp -R $securegenBlobs/motorola/osprey $cmVendor/motorola
+cp -R $securegenBlobs/proprietary_vendor_motorola/osprey $cmVendor/motorola
 breakfast osprey
 echo "Done."
 echo "osprey has been added."
 
 echo "Adding surnia..."
 breakfast surnia
-cp -R $securegenBlobs/motorola/msm8916-common $cmVendor/motorola
-cp -R $securegenBlobs/motorola/surnia $cmVendor/motorola
+cp -R $securegenBlobs/proprietary_vendor_motorola/msm8916-common $cmVendor/motorola
+cp -R $securegenBlobs/proprietary_vendor_motorola/surnia $cmVendor/motorola
 breakfast surnia
 echo "Done."
 echo "surnia has been added."
 
 echo "Adding victara..."
 breakfast victara
-cp -R $securegenBlobs/motorola/victara $cmVendor/motorola
+cp -R $securegenBlobs/proprietary_vendor_motorola/victara $cmVendor/motorola
 breakfast victara
 echo "Done."
 echo "victara has been added."
@@ -92,20 +92,20 @@ echo "All Motorola blobs have been added."
 
 #Samsung Blobs
 echo "Adding Samsung blobs..."
-mkdir $cmVendor/samsung
+mkdir $cmVendor/samsunsg
 
 echo "Adding klte..."
 breakfast klte
-cp -R $securegenBlobs/samsung/klte-common $cmVendor/samsung
-cp -R $securegenBlobs/samsung/klte $cmVendor/samsung
+cp -R $securegenBlobs/proprietary_vendor_samsung/klte-common $cmVendor/samsung
+cp -R $securegenBlobs/proprietary_vendor_samsung/klte $cmVendor/samsung
 breakfast klte
 echo "Done."
 echo "klte has been added."
 
 echo "Adding i9300..."
 breakfast i9300
-cp -R $securegenBlobs/samsung/i9300 $cmVendor/samsung
-cp -R $securegenBlobs/samsung/smdk4412-common $cmVendor/samsung
+cp -R $securegenBlobs/proprietary_vendor_samsung/i9300 $cmVendor/samsung
+cp -R $securegenBlobs/proprietary_vendor_samsung/smdk4412-common $cmVendor/samsung
 breakfast i9300
 echo "Done"
 echo "i9300 has been added."
@@ -117,8 +117,8 @@ mkdir $cmVendor/asus
 
 echo "Adding Z00A..."
 breakfast Z00A
-cp -R $securegenBlobs/asus/mofd-common $cmVendor/asus
-cp -R $securegenBlobs/asus/Z00A $cmVendor/asus
+cp -R $securegenBlobs/proprietary_vendor_asus/mofd-common $cmVendor/asus
+cp -R $securegenBlobs/proprietary_vendor_asus/Z00A $cmVendor/asus
 breakfast Z00A
 echo "Done."
 echo "Z00A has been added."
