be235935dbc464c56c6b40e2882aa0cfa54f67a5
Caio <caiocanic@outlook.com>
2016-11-16 18:16:07 -0200

    removePacks.sh now removes Gello by deleting it's dir.
    A more appropriate solution to #7 still need to be found.

--- removePacks.sh
+++ removePacks.sh
@@ -17,3 +17,6 @@ cd $cmHome
 bash $securegenPRA/gather.sh -y
 bash $securegenPRA/clean.sh -y
 bash $securegenPRA/catToBuild.sh -y
+
+#Since Gello isn't being removed by the packageRemovalAux scripts it will be removed manually
+rm -rf $cmHome/packages/apps/Gello
